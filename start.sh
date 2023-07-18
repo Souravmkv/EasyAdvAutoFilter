f146201a3b193083ab35a5b1d8e66a3f07ee0542
Mr.Spidy <128451864+spidyynaik@users.noreply.github.com>
2023-07-18 22:42:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-gunicorn app:app & python3 bot.py
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/spidyynaik/YDNewAuto.git /spidyauto
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /spidyauto
+fi
+cd /spidyauto
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
