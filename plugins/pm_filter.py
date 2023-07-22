3522865eaf25ffdc649a804b29169927914341e9
Mr.Spidy <128451864+spidyynaik@users.noreply.github.com>
2023-07-23 01:37:25 +0530

    sticker dlt

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2226,7 +2226,7 @@ async def auto_filter(client, msg, spoll=False):
     # else:
     fuk = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
     
-    await m.delete()
+    await dlt.delete()
     try:
         if settings['auto_delete']:
             await asyncio.sleep(300)
