5dab2e53f97faef17e9ba4b58402124ba7f036fb
Souravmkv <144803224+Souravmkv@users.noreply.github.com>
2023-09-12 18:25:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/spidyynaik/YDNewAuto.git /spidyauto
+  git clone https://github.com/Souravmkv/EasyAdvAutoFilter.git /spidyauto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /spidyauto
