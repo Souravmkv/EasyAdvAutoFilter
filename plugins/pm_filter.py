9fcb64ab6b834dfd739b24bdd07e49d1a0257e0d
Souravmkv <144803224+Souravmkv@users.noreply.github.com>
2023-09-30 11:51:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2049,8 +2049,9 @@ async def auto_filter(client, msg, spoll=False):
     pre = 'filep' if settings['file_secure'] else 'file'
     key = f"{message.chat.id}-{message.id}"
     FRESH[key] = search
+    FRESH[key] = search
     temp.GETALL[key] = files
-        temp.SHORT[message.from_user.id] = message.chat.id
+    temp.SHORT[message.from_user.id] = message.chat.id
     if settings["button"]:
         btn = [
             [
